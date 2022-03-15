8618a35cac06eb2c2cae2ae6bdf79b8f600b24bf
chutian0610 <victorchu0610@outlook.com>
2022-03-15 19:44:52 +0800

    json-benchmark

--- json-lib-benchmark/src/test/java/info/victorchu/jlb/data/RowsGeneratorTest.java
+++ json-lib-benchmark/src/test/java/info/victorchu/jlb/data/RowsGeneratorTest.java
@@ -1,14 +1,18 @@
 package info.victorchu.jlb.data;
 
 
+import com.alibaba.fastjson.JSON;
+import info.victorchu.jlb.JsonLibProvider;
 import org.junit.jupiter.api.Test;
 
 class RowsGeneratorTest {
 
     @Test
     void populate() {
-        Rows rows = new Rows();
-        new RowsGenerator().populate(rows,10000);
-        System.out.println(rows);
+        RowsGenerator rowsGenerator = new RowsGenerator();
+        Rows row1 = new Rows();
+        rowsGenerator.populate(row1,1000);
+        String result= JSON.toJSONString(row1);
+        System.out.println(new JsonLibProvider().jackson().deserialize(result,Rows.class));
     }
 }
